dd44a286628e7af4c71b77709a149c9af6b533e4
Phoenix-IT <dyachenko.yevhen@gmail.com>
2020-03-11 12:17:59 +0100

    abstract factory pattern completed

---
+++ creational_patterns/abstract_factory_pattern/app.py
@@ -0,0 +1,35 @@
+from abc import ABCMeta, abstractmethod
+from decimal import Decimal
+
+
+class AbstractFactory(metaclass=ABCMeta):
+    @abstractmethod
+    def build_sequence(self):
+        pass
+
+    @abstractmethod
+    def build_number(self, string):
+        pass
+
+
+class Factory:
+    def build_sequence(self):
+        return []
+
+    def build_number(self, string):
+        return Decimal(string)
+
+
+class Loader:
+    @staticmethod
+    def load(string, factory):
+        sequence = factory.build_sequence()
+        for substring in string.split(','):
+            item = factory.build_number(substring)
+            sequence.append(item)
+        return sequence
+
+
+f = Factory()
+result = Loader.load('1.23, 4.56', f)
+print(result)
