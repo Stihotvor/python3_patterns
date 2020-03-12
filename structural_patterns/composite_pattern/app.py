f144ba3a2706179a47d5629f8e982da07c6ed56e
Phoenix-IT <dyachenko.yevhen@gmail.com>
2020-03-12 11:19:04 +0100

    composite pattern created

---
+++ structural_patterns/composite_pattern/app.py
@@ -0,0 +1,18 @@
+class Widget:
+    def children(self):
+        return []
+
+
+# Frame has children and provide them once called
+class Frame(Widget):
+    def __init__(self, child_widgets):
+        self.child_widgets = child_widgets
+
+    def children(self):
+        return self.child_widgets
+
+
+# Label has no children in code implementation, but symmetrically returns empty list aka "no children"
+class Label:
+    def __init__(self, text):
+        self.text = text
